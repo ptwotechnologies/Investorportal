4c4528f4b111efb93aa964fadfd40cb2175e2197
Monika Sharma <monikasharma279826@gmail.com>
2026-03-24 10:24:04 +0530

    firebase key changes

--- Frontend/src/lib/firebase.js
+++ Frontend/src/lib/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCson-uYGTiL_0rFvEZv-wekNgXFR25Jh8",
-  authDomain: "ptwo-12cf9.firebaseapp.com",
-  projectId: "ptwo-12cf9",
-  storageBucket: "ptwo-12cf9.firebasestorage.app",
-  messagingSenderId: "263845171509",
-  appId: "1:263845171509:web:59aa346143a2fd4ee5fc7c",
-  measurementId: "G-C9Y003XK3M"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
